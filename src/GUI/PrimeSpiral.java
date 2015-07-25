eef3a62eaf0fda01be1ce221ec4d308fddfaf4e3
Ari Rappaport <arappaport@unm.edu>
2015-07-25 10:57:30 -0600

    working prime spiral

---
+++ src/GUI/PrimeSpiral.java
@@ -0,0 +1,93 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.LinkedList;
+
+public class PrimeSpiral extends JFrame { // implements Runnable{
+
+    public final int FRAME_WIDTH = 1200;
+    public final int FRAME_HEIGHT = 1200;
+    public final int NUM_ITERATIONS = 600;
+    public final int NUM_DIRECTIONS = 4;
+    //public final int SOME_NUMBER = 5;
+    private int[] xDir = {1, 0, -1, 0};
+    private int[] yDir = {0, 1, 0, -1};
+    private Point startTile = new Point((int)(FRAME_WIDTH/2), (int)(FRAME_HEIGHT/2));
+    private Point curTile;
+    LinkedList<Point> tilesToVisit = new LinkedList<Point>();
+
+    public PrimeSpiral() {
+
+        tilesToVisit.addFirst(curTile);
+        curTile = new Point();
+        curTile.setLocation(startTile.getX(),startTile.getY());
+        setTitle("PrimeSpiral");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(FRAME_WIDTH,FRAME_HEIGHT);
+        setVisible(true);
+
+    }
+    private void run(){
+
+        while(true){
+            repaint();
+        }
+
+    }
+    @Override
+    public void paint(Graphics g){
+
+            paintPrimes(g);
+
+    }
+
+    private void updateCoordinates(int curDir){
+
+        int dx = xDir[curDir];
+        int dy = yDir[curDir];
+        int curX = (int)curTile.getX();
+        int curY = (int)curTile.getY();
+        curTile.setLocation(curX+dx, curY+dy);
+
+    }
+
+    private boolean isItPrime(int number){
+
+        double numberToDouble = (double) number;
+        int sqrt = (int) Math.ceil(Math.sqrt(numberToDouble));
+        for(int i = 2; i <= sqrt; i++){
+          if(number % i == 0 && number != 2 || number == 1) return false;
+        }
+        return true;
+    }
+
+    private void paintPrimes(Graphics g){
+
+        int totalNum = 1;
+        for(int numLayers = 1; numLayers < NUM_ITERATIONS; numLayers++) {
+            curTile.setLocation(curTile.getX() - numLayers, curTile.getY() - numLayers);
+            for (int curDir = 0; curDir < NUM_DIRECTIONS; curDir++) {
+                for (int dist = 0; dist < 2 * numLayers; dist++) {
+                    updateCoordinates(curDir);
+                    if (isItPrime(totalNum)) {
+                        //if(totalNum % SOME_NUMBER == 1) g.setColor(Color.blue);
+                        //else g.setColor(Color.MAGENTA);
+                        g.drawLine((int) curTile.getX(), (int) curTile.getY(),
+                                   (int) curTile.getX(), (int) curTile.getY());
+                    }
+                    totalNum++;
+                }
+            }
+            curTile.setLocation(startTile.getX(),startTile.getY());
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        PrimeSpiral primeSpiral = new PrimeSpiral();
+        primeSpiral.run();
+
+    }
+}
--- src/Logic/GenerationWorker.java
+++ src/Logic/GenerationWorker.java
@@ -1,74 +1,52 @@
 package Logic;
 
-/*
-   Ari Rappaport
-   Student at UNM
-
-   Multithreaded Class that should run in parallel to reduce time
-   needed to generate large primes
- */
-public class GenerationWorker implements Runnable{
-
-    private String task;
-    private long number;
-    private long sqrt;
-    private long halfSqrt;
-    private static boolean foundFactor;
-
-
-    public GenerationWorker(String task, Long number){
-        this.task = task;
-        this.number = number;
-        double numberToDouble = number.doubleValue();
-        this.sqrt = (long) Math.ceil(Math.sqrt(numberToDouble));
-        this.halfSqrt = (long) Math.ceil((double) (sqrt/2));
-        foundFactor = false;
-    }
-    public static void main(String[] args){
-        GenerationWorker upper = new GenerationWorker("upper", Long.parseLong(args[0]));
-        GenerationWorker lower = new GenerationWorker("lower", Long.parseLong(args[0]));
-        Thread t1 = new Thread(upper);
-        Thread t2 = new Thread(lower);
-        t1.start();
-        t2.start();
-        if(foundFactor){
-            System.out.println("It's a prime alright!");
-        }
-        else{
-            System.out.println("Not a prime, I'm afraid.");
-        }
-    }
-
-    @Override
-    public void run(){
-        if((this.task).equals("upper")){
-            checkUpper(halfSqrt, sqrt);
-        }
-        if((this.task).equals("lower")){
-            checkLower(halfSqrt);
-        }
-    }
-
-    private synchronized void checkLower(long halfSqrt){
-
-        for(long i = 2; i <= halfSqrt; i++){
-
-            if(number % i == 0){
-                foundFactor = true;
-                return;
-            }
-        }
-    }
-
-    private synchronized void checkUpper(long halfSqrt, long sqrt){
-
-        for(long i = halfSqrt; i <= sqrt; i++){
-
-            if(number % i == 0){
-                foundFactor = true;
-                return;
-            }
-        }
-    }
-
+//import java.util.ArrayList;
+//import java.util.concurrent.BlockingQueue;
+//
+//import GUI.*;
+//
+///*
+//   Ari Rappaport
+//   Student at UNM
+//
+//   Multithreaded Class that should run in parallel to reduce time
+//   needed to generate large primes
+// */
+public class GenerationWorker{ //implements Runnable{
+//
+//    private long upperBound;
+//    private long number;
+//    private ArrayList<Long> primes;
+//    private BlockingQueue<Long> sharedPrimes = new BlockingQueue<Long>() {
+//    };
+//    private long sqrt;
+//
+//    public GenerationWorker(Long number){
+//        this.upperBound = number;
+//        double numberToDouble = number.doubleValue();
+//        this.sqrt = (long) Math.ceil(Math.sqrt(numberToDouble));
+//    }
+//
+//    public static void main(String[] args){
+//        GenerationWorker generator = new GenerationWorker(Long.parseLong(args[0]));
+//        PrimeSpiral gui = new PrimeSpiral();
+//        Thread t1 = new Thread(generator);
+//        Thread t2 = new Thread(gui);
+//        t1.start();
+//        t2.start();
+//    }
+//
+//    @Override
+//    public void run(){
+//        for(long i = 2; i <= upperBound; i++){
+//            findNextPrime(upperBound);
+//        }
+//    }
+//
+//    private void findNextPrime(long upperBound){
+//
+//
+//
+//    }
+//
 }
